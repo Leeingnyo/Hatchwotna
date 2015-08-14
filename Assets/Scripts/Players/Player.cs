221cb80fa828e7a1d1e3a8a616ce239966667615
Leeingnyo <dyd1928@naver.com>
2015-08-14 22:54:03 +0900

    make player

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int state;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void SetState(int state)
+    {
+        this.state = state;
+    }
+
+    public bool IsRest()
+    {
+        return this.state == (int)PlayerState.Normal;
+    }
+}
+
+enum PlayerState {
+    Normal = 0,
+    Attack,
+}

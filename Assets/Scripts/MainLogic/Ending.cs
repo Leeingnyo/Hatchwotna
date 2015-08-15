0c7d886a02f622294f29168fbbf1f390acf3c175
Leeingnyo <dyd1928@naver.com>
2015-08-15 23:42:08 +0900

    add ending player 1

---
+++ Assets/Scripts/MainLogic/Ending.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ending : MonoBehaviour {
+
+    float tic;
+
+	// Use this for initialization
+	void Start () {
+        tic = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tic += Time.deltaTime;
+        if (tic > 5f)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Application.LoadLevel(0);
+                //FIXME 나중에 메인으로 두기
+            }
+        }
+	}
+}
